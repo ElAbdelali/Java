b315fa92cd63eb99d7b9f293c35e10883b4d5d39
Ali Eljaouhari <eljaouhariabdelali@outlook.com>
2020-09-23 16:33:41 -0500

    First test code asking the user for an input and displaying it.

---
+++ test.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+import sun.java2d.pipe.SpanIterator; 
+
+public class test{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What would you like to print out utilizing this code?");
+        String phrase = scan.nextLine();
+        System.out.println(phrase);
+
+
+    }
+
+}
+
